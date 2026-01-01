06aedd3c7dbb9003149651a8c0870833968303ec
ASSASSInGHOST-dev <agnibha.paul.10.08.2006@gmail.com>
2026-01-01 23:34:55 +0530

    Add files via upload

--- array.c
+++ array.c
@@ -2,29 +2,20 @@
 
 int largest(int a[],int size) {
     int max=a[0];
-    for(int i=0;i<=size;i++) {
+    for(int i=0;i<size;i++) {
         if(max<a[i])
         max=a[i];
     }
     return max;
 }
 
-int sum(int a[],int size) {
-    int s=0;
-    for(int i=0;i<=size-1;i++)
-    s=s+a[i];
-    return s;
-}
-
-void reverse(int a[],int size) {
-    int b[size],s=0;
-    for(int i=size-1;i>=0;i--){
-        b[s]=a[i];
-        s++;
+int smallest(int a[],int size) {
+    int min=a[0];
+    for(int i=0;i<size;i++) {
+        if(min>a[i])
+        min=a[i];
     }
-    printf("The reversed array is:");
-    for(int j=0;j<=size-1;j++)
-    printf("%d",b[j]);
+    return min;
 }
 
 int main() {
@@ -32,13 +23,23 @@ int main() {
     printf("\nEnter number of elements in the array\n");
     scanf("%d",&n);
     int a[n];
+    int even=0,odd=0;
     printf("Enter array elements\n");
     for(int i=0;i<n;i++)
     scanf("%d",&a[i]);
     int l=largest(a,n);
-    int s=sum(a,n);
+    int min=smallest(a,n);
     printf("The largest element of the array is %d\n",l);
-    printf("The sum of the elements of the array is %d\n",s);
-    reverse(a,n);
+    printf("The smallest element of the array is %d\n",min);
+    for(int i=0;i<n;i++) {
+        if(a[i]!=0) {
+            if(a[i]%2==0)
+            even+=1;
+            else
+            odd+=1;
+        }
+    }
+    printf("The number of even numbers in the array is %d\n",even);
+    printf("The number of odd numbers in the array is %d\n",odd);
     return 0;
 }
