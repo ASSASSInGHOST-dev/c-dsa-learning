449ff24c89b5571f37bf93767ac38786958fce34
ASSASSInGHOST-dev <agnibha.paul.10.08.2006@gmail.com>
2026-01-11 00:07:27 +0530

    Add files via upload

---
+++ string_copy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char src[]="Copying String without using Strcopy()";
+    char dest[]="";
+    int count=0;
+    while(src[count]!='\0') {
+        dest[count]=src[count];
+        count++;
+    }
+    dest[count]='\0';
+    printf("Copied string is %s",dest);
+    return 0;
+}
